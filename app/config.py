0a84be9605fe05fa2b37b16844420c1fe59cac4e
DA MAC M1 152 <damacm1152@DAs-MacBook-Air.local>
2025-10-08 15:32:09 +0200

    incluuded new database

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
 class Config:
     SECRET_KEY = 'lkjhgfdsa'
-    MONGO_URI = "mongodb+srv://thabang23mthimkulu:qRCFByxJU2elUcXB@database.bdk2p.mongodb.net/Mydatabase"
+
+    MONGO_URI = "mongodb+srv://thabang23mthimkulu:qRCFByxJU2elUcXB@database.bdk2p.mongodb.net/Mydatabase?retryWrites=true&w=majority"
